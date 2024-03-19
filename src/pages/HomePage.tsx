5d05613b69b4c422c15c220fc976ddf643e4f7af
Kezota <keziameilany2005@gmail.com>
2024-03-19 10:08:02 +0700

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/navbar/Navbar";
+import "../styles/homePage.css";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
