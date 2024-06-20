a12240fff3406bd01919ca882569fab05a6d185d
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-06-21 01:30:56 +0700

    added membership page

---
+++ src/pages/MembershipPage/components/JoinMember.tsx
@@ -0,0 +1,25 @@
+import "../../../styles/joinMember.css";
+import joinMember from "../../../assets/joinMember.png";
+
+export default function JoinMember() {
+  return (
+    <section className="join-member">
+      <div className="join-member-detail">
+        <h3>Bersatu dalam Keberanian dan Mewujudkan Kebaikan</h3>
+        <p>
+          Dengan mendaftar sebagai member BeraniPeduli, Anda akan menjadi bagian
+          dari jaringan orang-orang inspiratif yang memiliki semangat dan
+          dedikasi yang sama dalam menyebarkan kebaikan.{" "}
+        </p>
+        <p>
+          Mari bergabung dengan membership Berani Peduli dan bersama-sama kita
+          bisa membuat perbedaan yang berarti!
+        </p>
+        <button>Join Member</button>
+      </div>
+      <div className="image">
+        <img src={joinMember} alt="Join Member Image" />
+      </div>
+    </section>
+  );
+}
--- src/pages/MembershipPage/index.tsx
+++ src/pages/MembershipPage/index.tsx
@@ -7,6 +7,8 @@ import Footer from "../../components/Footer";
 
 import headerImage from "../../assets/membership-header.png";
 import "../../styles/membershipPage.css";
+import WhyMember from "./components/WhyMember";
+import JoinMember from "./components/JoinMember";
 
 export default function MembershipPage() {
   // scroll to top on page load
@@ -22,6 +24,8 @@ export default function MembershipPage() {
         title="Membership Page"
         description="Bergabunglah dengan membership Beranipeduli dan jadilah bagian dari komunitas peduli yang lebih besar!"
       />
+      <WhyMember />
+      <JoinMember />
       <MembershipForm />
       <Footer />
     </div>
